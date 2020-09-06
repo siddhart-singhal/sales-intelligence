7d2226a6bfdee41ee4fa98d61f09999d66b490cd
siddhart-singhal <siddhart.singhal@appdirect.com>
2020-09-06 22:18:06 +0530

    Inital Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# How to run 
+### 1:- `npm i` in main directory
+### 2:- open client folder
+### 3:- in cmd of client folder run `npm i`
+### 4:- in cmd of client folder run `npm run build`
+### 5:- run `npm start`
---
+++ client/src/javascript/index.js
@@ -0,0 +1,224 @@
+import '../sass/styles.scss';
+
+var div = document.createElement('div');
+div.setAttribute('class', 'post block bc2');
+div.innerHTML = `<div class="header">
+    <div class="logo">
+        <a href="#">
+            <span class="logo-img" title="SLintel"></span>
+        </a>
+        <button type="button" class="btn btn-danger btn-demo">Book a Demo</button>
+    </div> 
+</div>
+<div class="banner"></div>
+
+<div class="container-fluid">
+    <div style="position:absolute">
+        <nav id="myScrollspy">
+            <ul class="nav nav-pills sidebar">
+                <li>
+                    <a class="dropdown-item left-pad" href="#section1">What is Sales Intelligence?</a>
+                    <ul>
+                        <li><a class="dropdown-item" href="#section11">Misconceptions About Sales Intelligence</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a  href="#section2">The Building Blocks of Sales Intelligence</a>
+                    <ul>
+                        <li>
+                            <a class="dropdown-item" href="#section21">Organizational Charts</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#section22">Job Posting Alerts</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#section23">Firmographics</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#section24">Psychographics</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#section25">Tech Stacks & Technographics</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#section26">Contract Renewal Alerts</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#section27">Company Updates</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="dropdown-item left-pad" href="#section3">The Benefits of Sales Intelligence: How Can Sales Intelligence Impact My Company?</a>
+                </li>
+                <li>
+                    <a href="#section4">Sales Intelligence: How Actionable Is It, Really?</a>
+                    <ul>
+                        <li><a class="dropdown-item" href="#section41">Buyer Intent</a></li>
+                        <li><a class="dropdown-item" href="#section42">Propensity Score</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div class="row strip-color">
+        <div class="col-sm-3 col-4"></div>
+        <div class="col-sm-9 col-8">
+            <div>    
+                <h1>Sales Intelligence: A Guide to Smart Selling</h1>
+                <p>If you asked someone what “sales intelligence” was a decade ago, they’d have mostly likely drawn a blank to your question.
+                 Sales intelligence as we know it today simply did not exist back then. While it was a requirement that sales teams of the past were yearning to find a solution for (as they do today), the technology to support the requirement was not easy to build.
+                 <br/><br/>
+                 Today, thanks to the thriving universe of SaaS products and the technological leaps that we’ve made, sales intelligence is powerful and accessible—enabling sales teams to sell better, faster, and with more insight than ever before.
+                 <br/><br/> But what is sales intelligence, really? What are the building blocks that it is made up of, how can it impact your company, and how will it evolve in the future? That’s exactly what we’ll be exploring today. Let’s dive right into it.</p>
+            </div>
+        </div>
+   </div>
+
+
+    <div class="row strip-color">
+        <div class="col-sm-3 col-4"></div>
+        <div class="col-sm-9 col-8 big-data">
+            <div id="section1"> 
+                <h2>What is Sales Intelligence?</h2>
+                <p>Sales intelligence, in a nutshell, refers to the information and insights that companies can have about their prospective buyers.<br/><br/>
+                As a sales representative, manager, or VP, you’d want to have the most up-to-date, relevant, and personalized information about your target account’s users, buyers, and decision-makers. The more information you have about your prospects, the more are your chances of closing the sale, just because of that fact that you were able to accurately determine when they want your product/service and why they should want it.<br/><br/> 
+               Sales intelligence helps you find the right kind of prospects for your company, enables you to reach them on the right platform, build a pitch that address their exact pain points, and shorten the time that it takes to close the deal.<br/><br/>
+                While that should give you a well-rounded idea of what sales intelligence is, we often find that several other things are often misconstrued as sales intelligence while they really aren’t. Let’s take a closer look at what those are.
+               </p>
+            </div> 
+            <div id="section11"> 
+                <h3>Misconceptions About Sales Intelligence</h3>
+                <p>Sales intelligence, in a nutshell, refers to the information and insights that companies can have about their prospective buyers.<br/><br/>
+                As a sales representative, manager, or VP, you’d want to have the most up-to-date, relevant, and personalized information about your target account’s users, buyers, and decision-makers. The more information you have about your prospects, the more are your chances of closing the sale, just because of that fact that you were able to accurately determine when they want your product/service and why they should want it.<br/><br/> 
+               Sales intelligence helps you find the right kind of prospects for your company, enables you to reach them on the right platform, build a pitch that address their exact pain points, and shorten the time that it takes to close the deal.<br/><br/>
+                While that should give you a well-rounded idea of what sales intelligence is, we often find that several other things are often misconstrued as sales intelligence while they really aren’t. Let’s take a closer look at what those are.
+               </p>
+            </div> 
+        </div>
+    </div>
+
+    <div class="row strip-color">
+        <div class="col-sm-3 col-4"></div>
+        <div class="col-sm-9 col-8">
+            <div id="section2"> 
+                <h1>The Building Blocks of Sales Intelligence</h1>
+                <p>Now that we’ve spoken about what sales intelligence is all about and how it can benefit your company, let’s dive into the different components that make up sales intelligence.<br/><br/>
+                1. Organizational Charts <br/>
+                2. Job Posting Alerts <br/>
+                3. Firmographics<br/>
+                4. Psychographics<br/>
+                5. Tech Stacks & Technographics <br/>
+                6. Contract Renewal Alerts <br/>
+                7. Company Updates Let’s break this down and take a deeper look at what each of these things mean.
+                </p>
+            </div> 
+        </div>
+    </div>
+    <div class="row strip-color">
+        <div class="col-sm-3 col-4"></div>
+        <div class="col-sm-9 col-8">
+            <div id="section21"> 
+                <h2>Organizational Charts</h2>
+                <p>Details for charts in this sections</p>
+            </div> 
+        </div>
+    </div>
+        <div class="row strip-color">
+        <div class="col-sm-3 col-4"></div>
+        <div class="col-sm-9 col-8">
+            <div id="section22"> 
+                <h2>Job Posting Alerts</h2>
+                <p>Details for job posting in this section</p>
+            </div> 
+        </div>
+    </div>
+    <div class="row strip-color">
+        <div class="col-sm-3 col-4"></div>
+        <div class="col-sm-9 col-8">
+            <div id="section23"> 
+                <h2>Firmographics</h2>
+                <p>Details for Firmographics in this section</p>
+            </div> 
+        </div>
+    </div>
+    <div class="row strip-color">
+        <div class="col-sm-3 col-4"></div>
+        <div class="col-sm-9 col-8">
+            <div id="section24"> 
+                <h2>Psychographics</h2>
+                <p>Details for Psychographics in this section</p>
+            </div> 
+        </div>
+    </div>
+    <div class="row strip-color">
+        <div class="col-sm-3 col-4"></div>
+        <div class="col-sm-9 col-8">
+            <div id="section25"> 
+                <h2>Tech stacks and Technographics</h2>
+                <p>Details for tech stacks in this section</p>
+            </div> 
+        </div>
+    </div>
+    <div class="row strip-color">
+        <div class="col-sm-3 col-4"></div>
+        <div class="col-sm-9 col-8">
+            <div id="section26"> 
+                <h2>Contact Renewal Alerts</h2>
+                <p>Details about contact renewal in this section</p>
+            </div> 
+        </div>
+    </div>
+    <div class="row strip-color">
+        <div class="col-sm-3 col-4"></div>
+        <div class="col-sm-9 col-8">
+            <div id="section27"> 
+                <h2>Company Updates</h2>
+                <p>Details about company updates in this section</p>
+            </div> 
+        </div>
+    </div>
+
+
+
+    <div class="row strip-color">
+        <div class="col-sm-3 col-4"></div>
+        <div class="col-sm-9 col-8">       
+            <div id="section3">         
+                <h1>The benefits of sales intelligence : How Can Sales Intelligence Impact My Company</h1>
+                <p>Details about sales intelligence in this section</p>
+            </div>
+        </div>
+    </div>
+    <div class="row strip-color">
+        <div class="col-sm-3 col-4"></div>
+        <div class="col-sm-9 col-8">
+            <div id="section4">         
+                <h1>Sales intelligence: How Actionable Is It, Really</h1>
+                <p>Details about sales intelligence in this section</p>
+            </div> 
+        </div>
+    </div>
+    <div class="row  strip-color">
+        <div class="col-sm-3 col-4"></div>
+        <div class="col-sm-9 col-8">     
+            <div id="section41">         
+                <h1>Buyer intent</h1>
+                <p>Details about buyer intent in this section</p>
+            </div>
+        </div>
+    </div>
+    <div class="row  strip-color">
+        <div class="col-sm-3 col-4"></div>
+        <div class="col-sm-9 col-8">     
+            <div id="section42">         
+                <h1>Propensity Score</h1>
+                <p>Details about propensity score in this section</p>
+            </div>
+        </div>
+    </div>
+
+  </div>
+</div>`;
+
+document.getElementById('parentID').append(div);
